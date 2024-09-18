81b8284e0395d10e9e9aa7611f7141d21e128cbb
kyuuji06 <jenardperonce0243@gmail.com>
2024-09-18 22:20:09 +0800

    modified:   lib/pages/cart_page.dart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -7,7 +7,7 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Center(
       child: Text(
-        "Cart"
+        "Store Cart "
       ),
     );
   }
